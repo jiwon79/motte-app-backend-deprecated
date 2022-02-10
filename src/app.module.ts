c4ce27de40db2921722eb0e95ae6059579c58899
jiwon79 <19-079@ksa.hs.kr>
2022-02-11 02:39:17 +0900

    fix: clear project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/users.entity';
 
 @Module({
-  imports: [
-    AppModule,
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [User],
-      synchronize: true,
-      logging: true,
-    }),
-  ],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,9 @@
-import { Controller, Delete, Get, Param } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { User } from './users.entity';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
   @Get()
-  findAll(): Promise<User[]> {
-    return this.usersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param() id: string): Promise<User> {
-    return this.usersService.findOne(id);
-  }
-
-  @Delete(':id')
-  async remove(@Param() id: string): Promise<void> {
-    await this.usersService.remove(id);
+  index() {
+    return 'user get endpoint';
   }
 }

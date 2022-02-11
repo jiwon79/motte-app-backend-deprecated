9632e0784ebd7d2778a333535a3d07a398823429
jiwon79 <19-079@ksa.hs.kr>
2022-02-11 20:03:33 +0900

    feat: add connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot(
+      process.env.NODE_ENV
+        ? {
+            url: process.env.DATABASE_URL,
+            type: 'postgres',
+            entities: ['dist/**/*.entity{.ts,.js}'],
+            synchronize: false,
+            autoLoadEntities: true,
+          }
+        : {
+            type: 'sqlite',
+            database: 'db',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+            logging: true,
+          },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

a3bf091154669efdaadaacf875d4d4e3785870f6
jiwon79 <19-079@ksa.hs.kr>
2022-02-13 03:55:56 +0900

    chore: add database migration config

---
+++ ormconfig.ts
@@ -0,0 +1,34 @@
+import { ConnectionOptions } from 'typeorm';
+
+const typeormConfig: ConnectionOptions = process.env.NODE_ENV
+  ? {
+      url: process.env.DATABASE_URL,
+      name: 'production',
+      type: 'postgres',
+      synchronize: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: [__dirname + '/dist/**/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/dist/src/migrations/**/*.{ts,js}'],
+      migrationsTableName: 'migrations',
+      cli: {
+        entitiesDir: __dirname + '/dist/**/*.entity.{js,ts}',
+        migrationsDir: __dirname + '/dist/migrations/',
+      },
+    }
+  : {
+      name: 'local',
+      type: 'sqlite',
+      database: 'db',
+      synchronize: false,
+      logging: true,
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      migrations: [__dirname + '/src/migrations/*.{ts,js}'],
+      cli: {
+        entitiesDir: __dirname + '/**/*.entity.{ts,js}',
+        migrationsDir: __dirname + '/src/migrations',
+      },
+    };
+
+export = typeormConfig;
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,32 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlansModule } from './plans/plans.module';
+import typeormConfig from '../ormconfig';
+
+console.log(typeormConfig);
+console.log(1);
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forRoot(
-      process.env.NODE_ENV
-        ? {
-            url: process.env.DATABASE_URL,
-            type: 'postgres',
-            entities: ['dist/**/*.entity{.ts,.js}'],
-            synchronize: false,
-            autoLoadEntities: true,
-            ssl: {
-              rejectUnauthorized: false,
-            },
-          }
-        : {
-            type: 'sqlite',
-            database: 'db',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: false,
-            logging: true,
-          },
-    ),
-    PlansModule,
-  ],
+  imports: [UsersModule, TypeOrmModule.forRoot(typeormConfig), PlansModule],
   controllers: [AppController],
   providers: [AppService],
 })

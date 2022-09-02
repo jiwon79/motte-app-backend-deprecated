81236c92beaaf10d59495a141d15099c81837804
jiwon79 <19-079@ksa.hs.kr>
2022-09-02 09:27:11 +0900

    :sparkles: Plan mock value 들 추가

--- test/plans/mockPlan.ts
+++ test/plans/mockPlan.ts
@@ -1,5 +1,6 @@
 import { CreatePlanDto } from 'src/plans/dto/create-plan.dto';
 import { UpdatePlanDto } from 'src/plans/dto/update-plan.dto';
+import { Plan } from 'src/plans/plan.entity';
 
 export const mockCreateDto: CreatePlanDto = {
   date: '2022-01-01',
@@ -7,17 +8,26 @@ export const mockCreateDto: CreatePlanDto = {
   tag: '[]',
 };
 
+export const mockCreateWithAllField: CreatePlanDto = {
+  date: '2022-01-02',
+  title: 'title1',
+  tag: '[]',
+  channel: 'channel',
+  location: 'location',
+  content: 'content',
+};
+
 export const mockUpdateDto: UpdatePlanDto = {
   date: '2022-01-02',
   title: 'new title',
 };
 
-export const mockPlan = {
+export const mockPlan: Plan = {
   id: 1,
   date: '2022-01-01',
   title: 'title',
   tag: '[]',
-  // location: '',
-  // channel: '',
-  // content: '',
+  location: null,
+  channel: null,
+  content: null,
 };

333f342012e375a04405846d60051b2aab3f1a34
jiwon79 <19-079@ksa.hs.kr>
2022-09-06 18:50:06 +0900

    :bug: typeorm config 버그 수정 (e2e test 에서 production config가 불렸음)

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,35 +1,37 @@
 import { ConnectionOptions } from 'typeorm';
 
-const typeormConfig: ConnectionOptions = process.env.NODE_ENV
-  ? {
-      url: process.env.DATABASE_URL,
-      type: 'postgres',
-      synchronize: false,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/src/migrations/*.{ts,js}'],
-      migrationsTableName: 'migrations',
-      cli: {
-        entitiesDir: __dirname + '/**/*.entity.{js,ts}',
-        migrationsDir: 'src/migrations/',
-      },
-    }
-  : {
-      type: 'postgres',
-      database: 'github-actions-motte-app',
-      username: 'postgres',
-      password: 'postgres',
-      port: 5432,
-      synchronize: false,
-      logging: true,
-      entities: [__dirname + '/**/*.entity.{ts,js}'],
-      migrations: [__dirname + '/src/migrations/*.{ts,js}'],
-      cli: {
-        entitiesDir: __dirname + '/**/*.entity.{ts,js}',
-        migrationsDir: 'src/migrations',
-      },
-    };
+const typeormConfig: ConnectionOptions =
+  process.env.ENVIROMENT === 'develop'
+    ? {
+        type: 'postgres',
+        database: 'github-actions-motte-app',
+        username: 'postgres',
+        password: 'postgres',
+        port: 5432,
+        synchronize: false,
+        logging: true,
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        migrations: [__dirname + '/src/migrations/*.{ts,js}'],
+        cli: {
+          entitiesDir: __dirname + '/**/*.entity.{ts,js}',
+          migrationsDir: 'src/migrations',
+        },
+        ssl: false,
+      }
+    : {
+        url: process.env.DATABASE_URL,
+        type: 'postgres',
+        synchronize: false,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/src/migrations/*.{ts,js}'],
+        migrationsTableName: 'migrations',
+        cli: {
+          entitiesDir: __dirname + '/**/*.entity.{js,ts}',
+          migrationsDir: 'src/migrations/',
+        },
+      };
 
 export = typeormConfig;
